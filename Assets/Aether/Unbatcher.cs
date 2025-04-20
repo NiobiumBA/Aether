bb5c7103385ca95e80bf04dad7a02278aa508243
NiobiumBA <berezhnoy1artem@yandex.ru>
2025-04-20 13:23:32 +0300

    Rewrite Unbatcher

--- Assets/Aether/Unbatcher.cs
+++ Assets/Aether/Unbatcher.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Aether
 {
@@ -9,92 +8,73 @@ namespace Aether
     /// </summary>
     public class Unbatcher
     {
-        private readonly Queue<ArraySegment<byte>> m_queue = new();
-        private readonly NetworkWriter m_writer = new();
-        private int m_offset = 0;
-        private bool m_readPacketSize = true;
-        private int m_packetSize;
+        private NetworkWriter m_writer = new();
+        private NetworkWriter m_secondWriter = new();
+        private bool m_isReadPacketInfoNext = true;
+        private int m_packetSize = 0;
 
         public void Enqueue(ArraySegment<byte> data)
         {
-            m_queue.Enqueue(data);
+            m_writer.WriteBytes(data);
         }
 
         public bool TryDequeue(out ArraySegment<byte> data)
         {
-            if (m_queue.Count == 0)
+            if (m_writer.Position == 0)
             {
                 data = ArraySegment<byte>.Empty;
                 return false;
             }
 
-            NetworkReader firstReader = new(m_queue.Peek());
+            NetworkReader reader = new(m_writer.ToArraySegment());
 
-            firstReader.ReadBytes(m_offset); // Skip bytes
+            bool isUpdateWriter = false;
 
-            if (m_readPacketSize)
+            if (m_isReadPacketInfoNext)
             {
-                m_packetSize = firstReader.ReadInt();
-                m_offset += Batcher.PacketInfoSize;
-                m_readPacketSize = false;
+                if (reader.Remaining < Batcher.PacketInfoSize)
+                {
+                    data = ArraySegment<byte>.Empty;
+                    return false;
+                }
+
+                isUpdateWriter = true;
+                m_packetSize = reader.ReadInt();
+
+                m_isReadPacketInfoNext = false;
             }
 
-            if (m_packetSize <= firstReader.Remaining)
+            if (m_packetSize <= reader.Remaining)
             {
-                if (m_packetSize == firstReader.Remaining)
-                {
-                    m_offset = 0;
-                    m_queue.Dequeue();
-                }
-                else
-                {
-                    m_offset += m_packetSize;
-                }
+                data = reader.ReadBytes(m_packetSize);
 
-                m_readPacketSize = true;
+                m_isReadPacketInfoNext = true;
+                m_packetSize = 0;
+                UpdateWriter(reader);
 
-                data = firstReader.ReadBytes(m_packetSize);
                 return true;
             }
 
-            return TryDequeuePartialPacket(out data, firstReader);
+            if (isUpdateWriter)
+                UpdateWriter(reader);
+
+            data = ArraySegment<byte>.Empty;
+            return false;
         }
 
         public void Clear()
         {
-            m_queue.Clear();
-            m_offset = 0;
-            m_readPacketSize = true;
+            m_writer.Clear();
+            m_secondWriter.Clear();
+            m_isReadPacketInfoNext = true;
+            m_packetSize = 0;
         }
 
-        private bool TryDequeuePartialPacket(out ArraySegment<byte> data, NetworkReader firstReader)
+        private void UpdateWriter(NetworkReader reader)
         {
-            if (m_queue.Count == 1)
-            {
-                m_readPacketSize = false;
-                data = ArraySegment<byte>.Empty;
-                return false;
-            }
-
-            int firstPartSize = firstReader.Remaining;
-            ArraySegment<byte> firstPacketPart = firstReader.ReadBytes(firstPartSize);
-
-            m_queue.Dequeue();
-
-            NetworkReader secondReader = new(m_queue.Peek());
-
-            int secondPartSize = m_packetSize - firstPartSize;
-            ArraySegment<byte> secondPacketPart = secondReader.ReadBytes(secondPartSize);
-
-            m_offset = secondPartSize;
-            m_readPacketSize = true;
-
             m_writer.Clear();
-            m_writer.WriteBytes(firstPacketPart);
-            m_writer.WriteBytes(secondPacketPart);
-
-            data = m_writer.ToArraySegment();
-            return true;
+            (m_writer, m_secondWriter) = (m_secondWriter, m_writer);
+            m_writer.WriteBytes(reader.ToArraySegment());
         }
     }
 }
